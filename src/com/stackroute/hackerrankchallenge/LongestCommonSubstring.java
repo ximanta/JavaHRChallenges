902a543738aec5454fe93e16ed4433da5a514a68
kaur007imneet <kaur007imneet@gmail.com>
2022-06-20 17:41:57 +0530

    added longest common substring in Suffix Array

---
+++ src/com/stackroute/hackerrankchallenge/LongestCommonSubstring.java
@@ -0,0 +1,25 @@
+package com.stackroute.hackerrankchallenge;
+
+/*
+A string is finite sequence of characters over a non-empty finite set Σ.
+In this problem, Σ is the set of lowercase letters.
+Substring, also called factor, is a consecutive sequence of characters occurrences at least once in a string.
+Now your task is simple, for two given strings, find the length of the longest common substring of them.
+Here common substring means a substring of two or more strings.
+
+Input
+The input contains exactly two lines, each line consists of no more than 250000 lowercase letters, representing a string.
+
+Output
+The length of the longest common substring. If such string doesn't exist, print "0" instead.
+
+Example
+Input:
+alsdfkjfjkdsal
+fdjskalajfkdsla
+
+Output:
+3
+ */
+public class LongestCommonSubstring {
+}
